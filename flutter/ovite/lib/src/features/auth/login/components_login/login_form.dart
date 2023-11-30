7a19cfc3d143e8053505c6196e4baba5270b925f
Sonny <sonny.luscap@gmail.com>
2023-11-30 23:26:04 +0100

    Initialisation de la branche + Mise en place du front login et register

---
+++ flutter/ovite/lib/src/features/auth/login/components_login/login_form.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../../../../shared/components/already_have_an_account_acheck.dart';
+import '../../../../shared/constants/constants.dart';
+// import '../../../Signup/signup_screen.dart';
+
+import '../../register/signup_page.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "Email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "Mot de passe",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Se Connecter".toUpperCase(),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter/ovite/lib/src/shared/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 57, 147, 250);
+const kPrimaryLightColor = Color.fromARGB(255, 245, 237, 255);
+
+const double defaultPadding = 16.0;
