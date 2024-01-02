7c1125d74280e00bb1d6430b73398d7eb01c2e82
Matteo Delandhuy <matteodelandhuypro@outlook.fr>
2024-01-02 15:24:09 +0100

    full crud api

---
+++ backend/src/api/complains/complains.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { robustoCrud } from 'src/services/database/database.service';
+import typia, { createAssertEquals } from 'typia';
+import { InsertDto, SelectDto, UpdateDto } from 'packages/robusto-dto/types';
+import { TypedParam, TypedRoute } from '@nestia/core';
+import { TId } from 'packages/robusto-crud/base-entity';
+import { ProductEntity } from 'src/services/database/entities/product.entity';
+import { ComplainEntity } from 'src/services/database/entities/complain.entity';
+
+type SelectComplainDto = SelectDto<ComplainEntity>;
+type InsertComplainDto = InsertDto<ComplainEntity>;
+type UpdateComplainDto = UpdateDto<ComplainEntity>;
+
+@Controller('complains')
+export class ComplainsController {
+  private readonly crudator = robustoCrud({
+    entityDB: ComplainEntity,
+    selectKeys: [...typia.misc.literals<keyof SelectComplainDto>()],
+    assertSelectDto: createAssertEquals<SelectComplainDto>(),
+    assertSelectDtoArray: createAssertEquals<SelectComplainDto[]>(),
+    assertInsertDto: createAssertEquals<InsertComplainDto>(),
+    assertUpdateDto: createAssertEquals<UpdateComplainDto>(),
+  });
+
+  @Get()
+  async getAll() {
+    return this.crudator.fetchAll();
+  }
+
+  @Post()
+  async create(@Body() data: InsertComplainDto) {
+    return this.crudator.insert(data);
+  }
+
+  @TypedRoute.Get(':id')
+  async getById(@TypedParam('id') id: TId) {
+    return this.crudator.fetchById(id);
+  }
+
+  @TypedRoute.Delete(':id')
+  async delete(@TypedParam('id') id: TId) {
+    return this.crudator.delete(id);
+  }
+
+  @TypedRoute.Patch()
+  async update(@TypedParam('id') id: TId, @Body() data: UpdateComplainDto) {
+    return this.crudator.patch(id, data);
+  }
+}
--- backend/src/api/users/users.controller.ts
+++ backend/src/api/users/users.controller.ts
@@ -1,17 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Orm, robustoCrud } from '../../services/database/database.service';
+import { robustoCrud } from '../../services/database/database.service';
 import { UserEntity } from 'src/services/database/entities/user.entity';
-import { TId, TOmitBaseEntity } from 'packages/robusto-crud/base-entity';
-import typia, { assert, createAssert, createAssertEquals } from 'typia';
+import { TId } from 'packages/robusto-crud/base-entity';
+import typia, { createAssertEquals } from 'typia';
 import { TypedParam, TypedRoute } from '@nestia/core';
-import { Except } from 'type-fest';
-import { RobustoHelper } from 'packages/robusto-crud/helpers';
-import { RemoveNever } from 'utils/types';
-import { ExceptWithoutRelations } from 'packages/robusto-dto/types';
+import { SelectDto, UpdateDto } from '../../../packages/robusto-dto/types';
 
-type UserSelectDto = ExceptWithoutRelations<UserEntity, 'password'>;
-type UserCreateDto = Pick<UserEntity, 'email' | 'password' | 'role'>;
-type UserUpdateDto = TOmitBaseEntity<UserEntity>;
+type UserSelectDto = SelectDto<UserEntity, 'password'>;
+export type UserCreateDto = Pick<UserEntity, 'email' | 'password' | 'role'>;
+type UserUpdateDto = UpdateDto<UserEntity>;
 
 @Controller('users')
 export class UsersController {
